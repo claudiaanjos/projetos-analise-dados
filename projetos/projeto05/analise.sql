c1204e3ff15dffd2029d6be0295ccb3a3171a7ac
Claudia <ndosanjosc@gmail.com>
2023-11-20 15:23:06 -0300

    script

--- projetos/projeto05/analise.sql
+++ projetos/projeto05/analise.sql
@@ -1,6 +1,6 @@
-/* -------------------------
-   Scripts do estudo de caso
-   -------------------------*/
+/* ----------
+   Análises
+   ---------*/
 
 -- Qual é o valor total gasto por cada cliente no restaurante?
 
@@ -16,8 +16,8 @@ GROUP BY cliente_id;
 -- Quantos dias cada cliente visitou o restaurante?
 
 SELECT 
-  	customer_id                 AS cliente_id,
-    count(DISTINCT order_date)  AS total_visitas 
+	customer_id                 AS cliente_id,
+	count(DISTINCT order_date)  AS total_visitas 
 FROM dannys_diner.sales sales
 GROUP BY cliente_id;
 
@@ -26,17 +26,17 @@ GROUP BY cliente_id;
 
 WITH primeiro_ranking AS (
 	SELECT 
-		sales.customer_id, 
-  		sales.order_date, 
-  		menu.product_name,
+		sales.customer_id,
+		sales.order_date,
+		menu.product_name,
 		dense_rank() OVER (PARTITION BY sales.customer_id ORDER BY sales.order_date) AS dr
 	FROM       dannys_diner.sales  sales
 	INNER JOIN dannys_diner.menu   menu 
 	ON sales.product_id = menu.product_id)
 
 SELECT 
-  	customer_id  AS cliente_id,
-    product_name AS primeiro_produto_adquirido 
+	customer_id  AS cliente_id,
+	product_name AS primeiro_produto_adquirido 
 FROM primeiro_ranking
 WHERE dr = 1
 GROUP BY cliente_id, primeiro_produto_adquirido;
@@ -45,7 +45,7 @@ GROUP BY cliente_id, primeiro_produto_adquirido;
 -- Qual é o item mais comprado no cardápio e quantas vezes foi pedido por todos os clientes?
 
 SELECT 
-	menu.product_name       AS "item mais comprado", 
+	menu.product_name       AS "item mais comprado",
 	count(sales.product_id) AS "quantidade de compras"
 FROM       dannys_diner.sales  sales
 INNER JOIN dannys_diner.menu   menu
@@ -59,18 +59,18 @@ LIMIT 1;
 
 WITH popular AS (
 	SELECT 
-  		sales.customer_id        AS cliente_id, 
-  		menu.product_name        AS produto, 
-  		count(sales.product_id)  AS quantidade_comprada,
+		sales.customer_id        AS cliente_id,
+		menu.product_name        AS produto,
+		count(sales.product_id)  AS quantidade_comprada,
 		dense_rank() OVER (PARTITION BY sales.customer_id ORDER BY count(sales.product_id) DESC) AS dr
 	FROM       dannys_diner.sales  sales
 	INNER JOIN dannys_diner.menu   menu 
 	ON sales.product_id = menu.product_id
-	GROUP BY sales.customer_id, menu.product_name);
+	GROUP BY sales.customer_id, menu.product_name)
 	
 SELECT 
-	cliente_id, 
-	produto, 
+	cliente_id,
+	produto,
 	quantidade_comprada
 FROM popular
 WHERE dr = 1;
@@ -80,18 +80,17 @@ WHERE dr = 1;
 
 WITH primeira_compra AS (
 	SELECT 
-  		sales.customer_id, 
-  		sales.product_id,
+		sales.customer_id,
+		sales.product_id,
 		row_number() OVER (PARTITION BY sales.customer_id ORDER BY sales.order_date) AS rn
 	FROM       dannys_diner.sales     sales
 	INNER JOIN dannys_diner.members   members  
 	ON sales.customer_id = members.customer_id AND
-	sales.order_date > members.join_date
-);
+	sales.order_date > members.join_date)
 
 SELECT 
-	customer_id  AS cliente_id, 
-    product_name AS produto
+	customer_id  AS cliente_id,
+	product_name AS produto
 FROM primeira_compra 
 INNER JOIN dannys_diner.menu   menu 
 ON primeira_compra.product_id = menu.product_id
@@ -103,19 +102,19 @@ ORDER BY cliente_id ASC;
 
 WITH comprado AS (
 	SELECT 
-  		sales.customer_id, 
-  		menu.product_name,
+		sales.customer_id,
+		menu.product_name,
 		dense_rank() OVER (PARTITION BY sales.customer_id ORDER BY sales.order_date DESC) AS dr
 	FROM       dannys_diner.sales  sales
 	INNER JOIN dannys_diner.menu   menu 
 	ON sales.product_id = menu.product_id
 	INNER JOIN dannys_diner.members members 
 	ON sales.customer_id = members.customer_id
-	WHERE sales.order_date < members.join_date);
+	WHERE sales.order_date < members.join_date)
 
 SELECT 
-	customer_id  AS cliente_id, 
-    product_name AS produto_comprado_antes_ser_membro
+	customer_id  AS cliente_id,
+	product_name AS produto_comprado_antes_ser_membro
 FROM comprado
 WHERE dr = 1;
 
@@ -134,6 +133,7 @@ ON sales.customer_id = members.customer_id
 WHERE sales.order_date < members.join_date
 GROUP BY cliente_id
 ORDER BY cliente_id ASC;
+
 
 /* 
 Se cada $1 gasto equivale a 10 pontos e o sushi tem um multiplicador de pontos de 2x, 
@@ -154,6 +154,7 @@ ON sales.product_id = menu.product_id
 GROUP BY cliente_id
 ORDER BY cliente_id ASC;
 
+
 /*
 Durante a primeira semana após a adesão de um cliente ao programa (incluindo a data de inscrição), 
 eles recebem pontos em dobro em todos os itens, não se limitando ao sushi. 
@@ -162,27 +163,27 @@ Ao considerar essa promoção, quantos pontos os clientes A e B acumulariam no f
 
 WITH pontuacao AS (
 	SELECT 
-    	sales.customer_id   AS cliente_id, 
-    	menu.product_name   AS produto, 
-    	menu.price          AS valor, 
-    	sales.order_date    AS data_pedido, 
-    	members.join_date   AS adesao, 
-    	join_date +6        AS uma_semana
+		sales.customer_id   AS cliente_id,
+		menu.product_name   AS produto,
+		menu.price          AS valor,
+		sales.order_date    AS data_pedido,
+		members.join_date   AS adesao,
+		join_date +6        AS uma_semana
   	FROM dannys_diner.sales AS sales
   	JOIN dannys_diner.menu  AS menu
     ON sales.product_id = menu.product_id
    	JOIN dannys_diner.members AS members
-    ON sales.customer_id = members.customer_id);
+    ON sales.customer_id = members.customer_id)
 
-SELECT
-  sum(CASE
+SELECT 
+	sum(CASE
     	WHEN data_pedido >= adesao AND data_pedido <= uma_semana 
 		THEN valor * 20
     	WHEN produto = 'sushi' 
 		THEN valor * 20
     	ELSE valor * 10
   	END) AS pontos,
-  cliente_id
+	cliente_id
 FROM pontuacao
 WHERE data_pedido < '2021-02-01'
 GROUP BY cliente_id;
@@ -192,11 +193,11 @@ GROUP BY cliente_id;
 
 CREATE VIEW status_membro AS
 SELECT 
-    sales.customer_id  AS cliente_id, 
-    sales.order_date   AS data_pedido, 
-    menu.product_name  AS produto, 
-    menu.price         AS valor,
-    CASE 
+	sales.customer_id  AS cliente_id,
+	sales.order_date   AS data_pedido,
+	menu.product_name  AS produto,
+	menu.price         AS valor,
+	CASE 
 		WHEN members.join_date > sales.order_date 
 		THEN 'Não'
         WHEN members.join_date <= sales.order_date 
